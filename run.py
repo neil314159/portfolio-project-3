8760f2878f0fc52ec1ce04b4caa3fd88f68db58b
neil314159 <neil@neilboland.com>
2021-12-31 13:01:18 +0000

    Test email validation library

--- run.py
+++ run.py
@@ -25,15 +25,7 @@ def main():
 
     data = info.get_all_values()        
 
-    while True:
-        print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
-
-        data_str = input("Enter your data here:\n")
-
-        sales_data = data_str.split(",")
-
+    response = pyip.inputEmail()
        
 
    
