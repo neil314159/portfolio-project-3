f2dd5d8b71affae503ee85f0dbe34714d3fef97a
neil314159 <neil@neilboland.com>
2021-12-31 12:55:16 +0000

    Add menu function to main screen

--- run.py
+++ run.py
@@ -18,9 +18,26 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cpm_data')
 
-info = SHEET.worksheet('data')
-
-data = info.get_all_values()
 
 
-print(data)
+def main():
+    info = SHEET.worksheet('data')
+
+    data = info.get_all_values()        
+
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here:\n")
+
+        sales_data = data_str.split(",")
+
+       
+
+   print(sales_data)
+
+
+print("Welcome!")
+main()
