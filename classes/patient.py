79bf82a43321a4661c3a19aa152e42b4e25bcebc
neil314159 <neil@neilboland.com>
2021-12-31 13:25:47 +0000

    Add instance variables to Patient class

--- classes/patient.py
+++ classes/patient.py
@@ -1,1 +1,18 @@
 #class for patient info
+
+from datetime import date
+
+
+class Patient:
+
+
+    def __init__(self):
+        self.firstname = "neil"
+        self.lastname = "boland"
+        self.dob = "1000"
+        self.firstdose = "TRUE"
+        self.seconddose = "TRUE"
+        self.booster = "TRUE"
+
+def age(self):
+    return date.today()
--- run.py
+++ run.py
@@ -18,13 +18,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cpm_data')
 
+class Patient:
 
+
+    def __init__(self):
+        self.firstname = "neil"
+        self.lastname = "boland"
+        self.dob = "1000"
+        self.firstdose = "TRUE"
+        self.seconddose = "TRUE"
+        self.booster = "TRUE"
+
+    def age(self):
+        return date.today()
 
 def main():
     info = SHEET.worksheet('data')
 
     data = info.get_all_values()        
-    calculate_ages(data)
+    #calculate_ages(data)
+    n = Patient()
+    print(n.age())
    # response = pyip.inputMenu(['cat', 'dog', 'moose'], numbered=True)
        
 
