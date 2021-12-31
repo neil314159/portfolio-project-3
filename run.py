2856cd4c11f409a866220ebf983e002765978988
neil314159 <neil@neilboland.com>
2021-12-31 14:24:27 +0000

    Add class for progress bar display

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import colorama
 from colorama import Fore, Back, Style
-from datetime import date
+from datetime import datetime
 import pyinputplus as pyip
 
 colorama.init(autoreset=True)
@@ -24,24 +24,33 @@ class Patient:
     def __init__(self):
         self.firstname = "neil"
         self.lastname = "boland"
-        self.dob = "1/2/1980"
+        self.dob = "1/2/1990"
         self.firstdose = "TRUE"
         self.seconddose = "TRUE"
         self.booster = "TRUE"
 
     def age(self):
         newdate = self.dob.split('/')
-        days = newdate[0]
-        months = newdate[1]
-        years = newdate[2]
+        days = int(newdate[0])
+        months = int(newdate[1])
+        years = int(newdate[2])
         
+        today = datetime.today()
 
-        datetime_str = '09/19/18'
+        difference = ((today.month, today.day) < (months, days))
+        
+        bday = today.year - years - int(difference)
+       
+       
+        return bday
 
-        datetime_object = datetime.strptime(datetime_str, '%m/%d/%y')
-        return years
-
-
+class ProgressBar:
+    def __init__(self):
+        self.percent_complete = 1
+    
+    def print_progress():
+        #
+        return ""
 
 def main():
     info = SHEET.worksheet('data')
