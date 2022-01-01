fb6b19ebb6a30c282f59ad4783ad61a38e13e7d7
neil314159 <neil@neilboland.com>
2022-01-01 13:31:04 +0000

    Add ternary operator to shorten table construction function

--- classes/tableview.py
+++ classes/tableview.py
@@ -9,25 +9,28 @@ class TableView():
         
         x = PrettyTable()
         x.field_names = ["Number", "First Name", "Last Name", "Age", "1st Dose", "2nd Dose", "Booster"]
-        x.max_width = 60
-        for a in self.patient_data[1:11]:
+        x._max_width = {"Number":10, "First Name":20, "Last Name":20, "Age":10, "1st Dose":8, "2nd Dose":8, "Booster":8}
+        
+        for a in self.patient_data[1:]:
             # print(getattr(a, 'firstname'))
             new_row = []
             new_row.append(1234)
             new_row.append(getattr(a, 'firstname'))
             new_row.append(getattr(a, 'lastname'))
             new_row.append(a.age())
-            new_row.append("✅")
-            new_row.append("❌")
-            new_row.append("✅")
-            # new_row.append(getattr(a, 'firstname'))
-            # new_row.append(getattr(a, 'firstname'))
+            
+
+            new_row.append("✅") if((getattr(a, 'first_dose'))=="TRUE") else new_row.append("❌")
+            new_row.append("✅") if((getattr(a, 'second_dose'))=="TRUE") else new_row.append("❌")
+            new_row.append("✅") if((getattr(a, 'booster_dose'))=="TRUE") else new_row.append("❌")
+           
             x.add_row(new_row)
         
         
         # print("+" + ("-")*8 + "+" + ("-")*20 + "+" + ("-")*20 + "+" + ("-")*8 + "+" + ("-")*6 + "+" + ("-")*6 + "+" + ("-")*6 + "+")
         
-        print(x)
+        # print(x)
+        print(x.get_string(start=1,end=4))
         
 
 
