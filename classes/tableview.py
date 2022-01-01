577aa204acdc29cc09a3b3ba195818136c5976d9
neil314159 <neil@neilboland.com>
2022-01-01 12:48:27 +0000

    Add new library to improve print formatting of patient table output

--- classes/tableview.py
+++ classes/tableview.py
@@ -1,10 +1,22 @@
+from prettytable import PrettyTable
+
 class TableView():
     def __init__(self, patient_data, page_number):
         self.patient_data = patient_data
         self.page_number = page_number
     
     def print_table(self):
-        print(self.patient_data[1].age())
+        
+        x = PrettyTable()
+        x.field_names = ["First Name", "Last Name", "Age", "First Dose"]
+        for a in self.patient_data[1:11]:
+            # print(getattr(a, 'firstname'))
+            x.add_row([getattr(a, 'firstname'), getattr(a, 'lastname'), a.age(), getattr(a, 'second_dose')])
+        
+        
+        
+        print(x)
+        
     
     def next_page(self):
         page_number +=1
