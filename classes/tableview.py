1ff66717c2f54efb7bf26f726f7d41cc5b8cc2c0
neil314159 <neil@neilboland.com>
2022-01-01 14:53:23 +0000

    Remove rich text formatting from tables due to alingment issues

--- classes/tableview.py
+++ classes/tableview.py
@@ -1,8 +1,5 @@
 from prettytable import PrettyTable
 from termcolor import colored, cprint
-from tabulate import tabulate
-from rich.table import Table
-from rich import print
 
 class TableView():
     def __init__(self, patient_data, page_number):
@@ -25,30 +22,17 @@ class TableView():
             
 
             new_row.append("YES") if((getattr(a, 'first_dose'))=="TRUE") else new_row.append("NO")
-            new_row.append("YES") if((getattr(a, 'second_dose'))=="TRUE") else new_row.append("N")
-            new_row.append("YES") if((getattr(a, 'booster_dose'))=="TRUE") else new_row.append("N")
+            new_row.append("YES") if((getattr(a, 'second_dose'))=="TRUE") else new_row.append("NO")
+            new_row.append("YES") if((getattr(a, 'booster_dose'))=="TRUE") else new_row.append("NO")
            
             x.add_row(new_row)
         
         
         # print("+" + ("-")*8 + "+" + ("-")*20 + "+" + ("-")*20 + "+" + ("-")*8 + "+" + ("-")*6 + "+" + ("-")*6 + "+" + ("-")*6 + "+")
         
-
-        table = Table(title="Star Wars Movies")
-
-        table.add_column("Released", justify="right", style="cyan", no_wrap=True)
-        table.add_column("Title", style="magenta")
-        table.add_column("Box Office", justify="right", style="green")
-
-        table.add_row("Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$952,110,690")
-        table.add_row("May 25, 2018", "✅", "$393,151,347")
-        table.add_row("Dec 15, 2017", "Star Wars Ep. V111: The Last Jedi", "$1,332,539,889")
-        table.add_row("Dec 16, 2016", "Rogue One: A Star Wars Story", "$1,332,439,889")
-
-        print(table)
        
         print(x)
-        #print(x.get_string(start=1,end=6))
+        print(x.get_string(start=1,end=6))
         
 
 
